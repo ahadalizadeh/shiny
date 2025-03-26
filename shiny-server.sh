07be25df99a09d691b8d72b9b44b6dfd7d57484e
ahadalizadeh <st.alizadeh@gmail.com>
2025-03-26 03:35:18 +0330

    e

--- shiny-server.sh
+++ shiny-server.sh
@@ -4,11 +4,7 @@
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
 
-mkdir -p  /srv/shiny-server/app1
-mkdir -p  /srv/shiny-server/app2
 
-cp -r /srv/apps/app1 /srv/shiny-server/app1
-cp -r /srv/apps/app2 /srv/shiny-server/app2
 
 
 if [ "$APPLICATION_LOGS_TO_STDOUT" != "false" ];
@@ -17,5 +13,6 @@ then
     exec xtail /var/log/shiny-server/ &
 fi
 
+cp -r /srv/apps/* /srv/shiny-server/
 # start shiny server
 exec shiny-server 2>&1
